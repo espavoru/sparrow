b1749ea1657b237281acbe6df860499dce61693e
Oleg <espavoru@gmail.com>
2019-07-20 12:41:55 +0300

    Додав та підключив меню

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'style_theme');
+add_action('wp_enqueue_scripts', 'header_scripts');
 add_action('wp_footer', 'scripts_theme');
+add_action('after_setup_theme', 'theme_register_nav_menu');
 
 function style_theme() {
     wp_enqueue_style('style', get_stylesheet_uri());
@@ -16,6 +18,14 @@ function scripts_theme() {
     wp_enqueue_script('jquery-migrate-1.2.1.min.js', get_template_directory_uri() . '/assets/js/jquery-migrate-1.2.1.min.js');
     wp_enqueue_script('jquery.flexslider.js', get_template_directory_uri() . '/assets/js/jquery.flexslider.js');
     wp_enqueue_script('doubletaptogo.js', get_template_directory_uri() . '/assets/js/doubletaptogo.js');
-    wp_enqueue_script('modernizr.js', get_template_directory_uri() . '/assets/js/modernizr.js');
     wp_enqueue_script('init.js', get_template_directory_uri() . '/assets/js/init.js');
 };
+
+function header_scripts() {
+    wp_enqueue_script('modernizr.js', get_template_directory_uri() . '/assets/js/modernizr.js');
+}
+
+function theme_register_nav_menu() {
+	register_nav_menu( 'top', 'Header menu' );
+	register_nav_menu( 'bottom', 'Footer menu' );
+}
--- header.php
+++ header.php
@@ -37,34 +37,21 @@
          <div class="twelve columns">
 
             <div class="logo">
+               <a href="<?php bloginfo("url") ?>"><h1 class="text-logo" style="color: #fff;"><?php bloginfo("name"); ?></h1></a>
                <a href="index.html"><img alt="" src="images/logo.png"></a>
             </div>
 
             <nav id="nav-wrap">
 
+               <?php wp_nav_menu(array(
+                  'theme_location' => 'top',
+                  'container' => null,
+                  'menu_class' => 'nav',
+                  'menu_id' => 'nav'
+               )); ?>
+
                <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	            <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-
-               <ul id="nav" class="nav">
-
-	               <li class="current"><a href="index.htm">Home</a></li>
-	               <li><span><a href="blog.html">Blog</a></span>
-                     <ul>
-                        <li><a href="blog.html">Blog Index</a></li>
-                        <li><a href="single.html">Post</a></li>
-                     </ul>
-                  </li>
-                  <li><span><a href="portfolio-index.html">Portfolio</a></span>
-                     <ul>
-                        <li><a href="portfolio-index.html">Portfolio Index</a></li>
-                        <li><a href="portfolio.html">Portfolio Entry</a></li>
-                     </ul>
-                  </li>
-	               <li><a href="about.html">About</a></li>
-                  <li><a href="contact.html">Contact</a></li>
-                  <li><a href="styles.html">Features</a></li>
-
-               </ul> <!-- end #nav -->
 
             </nav> <!-- end #nav-wrap -->
 
